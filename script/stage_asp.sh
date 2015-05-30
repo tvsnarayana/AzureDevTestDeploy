77e5b8ef1b89ce7432b6ae9c3c1596300c7f0eeb
Ross Gardler <ross.gardler@microsoft.com>
2015-05-30 15:33:53 -0700

    make version numbers configurable for both java and asp projects

--- script/stage_asp.sh
+++ script/stage_asp.sh
@@ -4,13 +4,13 @@
 
 source script/config.sh
 
-echo "Staging ASP application version $STAGE_VERSION on $STAGE_MACHINE_NAME"
+echo "Staging ASP application version $ASP_STAGE_VERSION on $STAGE_MACHINE_NAME"
 eval "$(docker-machine env $STAGE_MACHINE_NAME)"
 
 cd asp
 
 # Build the container to ensure we pick up any changes
-docker build -f stage-Dockerfile -t asp:$STAGE_VERSION .
+docker build -f stage-Dockerfile -t asp:$ASP_STAGE_VERSION .
 
 # Stop, remove and restart the container
 echo "Stopping any running staged container"
@@ -18,4 +18,4 @@ docker stop stage_asp
 echo "Removing any previously staged container"
 docker rm stage_asp
 echo "Running a container"
-docker run -t -d -p 8080:8080 --name=stage_asp asp:$STAGE_VERSION
+docker run -t -d -p 8080:8080 --name=stage_asp asp:$ASP_STAGE_VERSION
--- script/stage_java.sh
+++ script/stage_java.sh
@@ -1,11 +1,16 @@
 ###########################################
 # Build and stage the Java application
 ###########################################
-eval "$(docker-machine env tutorialStage)"
-docker-machine env tutorialStage
+
+source script/config.sh
+
+echo "Staging Java application version $JAVA_STAGE_VERSION on $STAGE_MACHINE_NAME"
+eval "$(docker-machine env $STAGE_MACHINE_NAME)"
+
+cd java
 
 # Build the container to ensure we pick up any changes
-docker build -t java .
+docker build -t java:$JAVA_STAGE_VERSION .
 
 # Stop, remove and restart the container
 echo "Stopping any running staged container"
@@ -13,4 +18,4 @@ docker stop stage_java
 echo "Removing any previously staged container"
 docker rm stage_java
 echo "Running a container"
-docker run -t -d -p 8080:8080 --name=stage_java java
+docker run -t -d -p 5050:8080 --name=stage_java java:$JAVA_STAGE_VERSION
