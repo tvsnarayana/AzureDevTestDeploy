4a8379d9a52d280e8f412a5a205f31eee47153bd
Ross Gardler <ross.gardler@microsoft.com>
2015-06-21 22:34:18 -0700

    unify on version number variable names

--- script/stage_asp.sh
+++ script/stage_asp.sh
@@ -9,13 +9,13 @@
 
 source script/config.sh
 
-echo "Staging ASP application version $ASP_STAGE_VERSION on $STAGE_MACHINE_NAME"
+echo "Staging ASP application version $WEB_STAGE_VERSION on $STAGE_MACHINE_NAME"
 eval "$(docker-machine env $STAGE_MACHINE_NAME)"
 
 cd asp
 
 # Build the container to ensure we pick up any changes
-docker build -t web:$ASP_STAGE_VERSION .
+docker build -t web:$WEB_STAGE_VERSION .
 
 # Stop, remove and restart the container
 echo "Stopping any running (staged) web application container"
@@ -23,6 +23,6 @@ docker stop stage_web
 echo "Removing any previously (staged) web application container"
 docker rm stage_web
 echo "Running a container"
-docker run -t -d -p 80:80 --link stage_rest:rest --name=stage_web web:$ASP_STAGE_VERSION
+docker run -t -d -p 80:80 --link stage_rest:rest --name=stage_web web:$WEB_STAGE_VERSION
 
 cd ..
