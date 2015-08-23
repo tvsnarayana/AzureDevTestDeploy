5f04d2c7ed75879d21721f3249a3ac77d3b1dae6
Ross Gardler <rogardle@microsoft.com>
2015-08-23 01:27:32 -0700

    don't add '-master' to staging name as it breaks some of the other scripts

--- script/create_stage_swarm.sh
+++ script/create_stage_swarm.sh
@@ -12,8 +12,8 @@ echo "Creating a Swarm cluster, using Docker Hub as the registry"
 sid=$(docker run swarm create)
 echo "Swarm cluster ID is " $sid
 
-echo "Creating Swarm Master as ${STAGE_MACHINE_NAME}-master" 
-docker-machine create -d azure --azure-location="$AZURE_LOCATION" --azure-subscription-id="$AZURE_SUBSCRIPTION_ID" --azure-subscription-cert="$AZURE_CERT_NAME" --swarm --swarm-master --swarm-discovery token://$sid  ${STAGE_MACHINE_NAME}-master
+echo "Creating Swarm Master as ${STAGE_MACHINE_NAME}" 
+docker-machine create -d azure --azure-location="$AZURE_LOCATION" --azure-subscription-id="$AZURE_SUBSCRIPTION_ID" --azure-subscription-cert="$AZURE_CERT_NAME" --swarm --swarm-master --swarm-discovery token://$sid  ${STAGE_MACHINE_NAME}
 
 echo "Creating a Swarm nodes named ${STAGE_MACHINE_NAME}-node-01"      
 docker-machine create -d azure --azure-location="$AZURE_LOCATION" --azure-subscription-id="$AZURE_SUBSCRIPTION_ID" --azure-subscription-cert="$AZURE_CERT_NAME" --swarm --swarm-discovery token://$sid ${STAGE_MACHINE_NAME}-node-01
