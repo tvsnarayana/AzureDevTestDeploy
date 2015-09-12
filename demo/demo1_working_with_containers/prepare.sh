454b6d9cd1ebe71a961ea7d109a914afdeef830d
Ross Gardler <ross@gardler.org>
2015-09-12 09:50:05 +0000

    remove images we want to see pulled

--- demo/demo1_working_with_containers/prepare.sh
+++ demo/demo1_working_with_containers/prepare.sh
@@ -1,5 +1,6 @@
 tmux kill-session -t azurecon-demo1
 docker stop $(docker ps -aq)
 docker rm $(docker ps -qa) 
-docker rmi $(docker images –q)
+docker rmi -f hello-world
+docker rmi -f tutum/hello-world
 mux azurecon-demo1
