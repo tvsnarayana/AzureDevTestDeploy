5962ce9b1a72fdb85fbe638065a3b5fe2e9cd993
Ross Gardler <ross@gardler.org>
2015-09-14 02:38:52 +0000

    ensure there are no stray containers and ensure we recreate when running the demo (sometimes the proxy doesn't recover well

--- demo/demo3_docker_compose/prepare.sh
+++ demo/demo3_docker_compose/prepare.sh
@@ -1,7 +1,10 @@
 tmux kill-session -t azurecon-demo3
 
-docker-compose up -d --force-recreate
+docker stop $(docker ps -q)
+
+docker-compose up -d
 docker-compose scale web=1
 docker-compose stop
+docker-compose rm -f
 
 mux azurecon-demo3
