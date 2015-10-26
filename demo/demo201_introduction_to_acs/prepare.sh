be2d2c6e51cbaa2ffea89be2f05af8dc9d0ad23b
Ross Gardler <ross@gardler.org>
2015-10-26 06:04:29 +0000

    initial ACS demo details

---
+++ demo/demo201_introduction_to_acs/prepare.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Demo parameters
+DEMO_NAME=acs-demo201
+
+# Demo script location
+SCRIPT_PATH=$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")
+DEMO_HOME=$(dirname $SCRIPT_PATH)
+
+# Configure TMux and Tmuxinator
+mkdir ~/.tmuxinator
+cp $DEMO_HOME/.tmuxinator/* ~/.tmuxinator
+
+# Configure Docker
+export DOCKER_HOME=
+export DOCKER=
+
+# Prepare Docker
+
+# Create tmux session
+tmux kill-session -t $DEMO_NAME
+mux $DEMO_NAME
---
+++ demo/demo201_introduction_to_acs/readme.md
@@ -0,0 +1,12 @@
+# Objectives
+
+1. Describe the objectives in bullets here
+2. This readme file will become a presentation
+
+---
+
+# .tmuxinator
+
+# commands.txt
+
+# prepare.sh
