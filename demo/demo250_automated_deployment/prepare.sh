18105cdbaef2517f7e5bc7f40f802f45b0b23839
Ross Gardler <ross@gardler.org>
2015-11-05 23:03:08 -0800

    clean up in order to prevent buggy start

--- demo/demo250_automated_deployment/prepare.sh
+++ demo/demo250_automated_deployment/prepare.sh
@@ -16,9 +16,8 @@ export DOCKER_HOME=
 export DOCKER=
 
 docker stop $(docker ps -q)
-
-# Bring up the demo application
-docker-compose -f docker-compose-dev.yml up -d 
+docker-compose -f docker-compose-dev.yml rm -f
+docker-compose -f docker-compose-dev.yml up -d
 
 # Create tmux session
 tmux kill-session -t $DEMO_NAME
